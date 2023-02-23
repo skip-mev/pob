d6a3a8e1877187201e22d0b42d7efad82de29a6d
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-02-23 15:25:44 -0500

    updates

--- mempool/mempool.go
+++ mempool/mempool.go
@@ -1,10 +1,29 @@
 package mempool
 
 import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkmempool "github.com/cosmos/cosmos-sdk/types/mempool"
 )
 
 var _ sdkmempool.Mempool = (*AuctionMempool)(nil)
 
 type AuctionMempool struct {
+}
+
+func (am *AuctionMempool) Insert(context.Context, sdk.Tx) error {
+	panic("not implemented")
+}
+
+func (am *AuctionMempool) Select(context.Context, [][]byte) sdkmempool.Iterator {
+	panic("not implemented")
+}
+
+func (am *AuctionMempool) CountTx() int {
+	panic("not implemented")
+}
+
+func (am *AuctionMempool) Remove(sdk.Tx) error {
+	panic("not implemented")
 }
