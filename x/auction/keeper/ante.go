8807c784a2da21aabd52d63550134123dbc68241
David Terpay <david.terpay@gmail.com>
2023-03-08 19:05:14 -0500

    updating docs in ante

--- x/auction/keeper/ante.go
+++ x/auction/keeper/ante.go
@@ -21,24 +21,15 @@ func NewAuctionDecorator(ak Keeper) AuctionDecorator {
 	}
 }
 
-// AnteHandle is the ante handler for the auction module. It validates that the auction bid is valid if one exists.
+// AnteHandle validates that the auction bid is valid if one exists.
 func (ad AuctionDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
-	maxBundleSize, err := ad.auctionKeeper.GetMaxBundleSize(ctx)
-	if err != nil {
-		return ctx, err
-	}
-
-	if maxBundleSize == 0 {
-		return next(ctx, tx, simulate)
-	}
-
 	// Extract the auction bid from the transaction if one exists.
 	auctionMsg, err := mempool.GetMsgAuctionBidFromTx(tx)
 	if err != nil {
 		return ctx, err
 	}
 
-	// If a bid exists, validate it and add it to the mempool.
+	// Validate the auction bid if one exists.
 	if auctionMsg != nil {
 		if err := ad.auctionKeeper.ValidateAuctionMsg(ctx, auctionMsg); err != nil {
 			return ctx, errors.Wrap(err, "failed to validate auction bid")
@@ -82,44 +73,50 @@ func (k Keeper) ValidateAuctionMsg(ctx sdk.Context, msg *types.MsgAuctionBid) er
 }
 
 // ValidateBundle validates the ordering of the referenced transactions. Bundles are valid if
-// 1. all of the transactions are signed by the signer.
-// 2. the first transactions are signed by the same party (not sender), and the subsequent txs are signed by the bidder.
+//  1. all of the transactions are signed by the signer.
+//  2. some subset of contiguous transactions are signed by the signer (starting from the front), and all other tranasctions
+//     are signed by the bidder.
+//
+// example:
+//  1. valid: [tx1, tx2, tx3] where tx1 is signed by the signer 1 and tx2 and tx3 are signed by the bidder.
+//  2. valid: [tx1, tx2, tx3, tx4] where tx1 - tx4 are signed by the bidder.
+//  3. invalid: [tx1, tx2, tx3] where tx1 is signed by the signer 1 and tx2 is signed by the bidder, and tx3 is signed by the signer 2. (possible sandwich attack)
+//  4. invalid: [tx1, tx2, tx3] where tx1 is signed by the bidder, and tx2 - tx3 is signed by the signer 1. (possible front-running attack)
 func (k Keeper) ValidateBundle(ctx sdk.Context, transactions [][]byte, bidder sdk.AccAddress) error {
 	if len(transactions) <= 1 {
 		return nil
 	}
 
-	// Get the signers of the first transaction and check if the bidder is one of them.
-	authorizedSigners, err := k.getTxSigners(transactions[0])
+	// prevSigners is used to track whether the signers of the current transaction overlap.
+	prevSigners, err := k.getTxSigners(transactions[0])
 	if err != nil {
 		return err
 	}
-	seenBidder := authorizedSigners[bidder.String()]
+	seenBidder := prevSigners[bidder.String()]
 
 	// Check that all subsequent transactions are signed by either
-	// 1. the same party as the first transaction, or
-	// 2. the bidder.
+	// 1. the same party as the first transaction
+	// 2. the same party for some arbitrary number of txs and then are all remaining txs are signed by the bidder.
 	for _, txbytes := range transactions[1:] {
-
 		txSigners, err := k.getTxSigners(txbytes)
 		if err != nil {
 			return err
 		}
 
-		// Check that all transactions are signed by the same bidder.
-		filterSigners(authorizedSigners, txSigners)
+		// Filter the signers to only those that signed the current transaction.
+		filterSigners(prevSigners, txSigners)
 
-		// If there are no authorized signers and the bidder address has not been seen, then the bundle can still be valid
+		// If there are no current signers and the bidder address has not been seen, then the bundle can still be valid
 		// as long as all subsequent transactions are signed by the bidder.
-		if len(authorizedSigners) == 0 {
+		if len(prevSigners) == 0 {
 			if seenBidder {
 				return fmt.Errorf("bundle contains transactions signed by multiple parties. bundle must be signed by the same party.")
 			} else {
 				seenBidder = true
-				authorizedSigners = map[string]bool{bidder.String(): true}
-				filterSigners(authorizedSigners, txSigners)
+				prevSigners = map[string]bool{bidder.String(): true}
+				filterSigners(prevSigners, txSigners)
 
-				if len(authorizedSigners) == 0 {
+				if len(prevSigners) == 0 {
 					return fmt.Errorf("bundle contains transactions signed by multiple parties. bundle must be signed by the same party.")
 				}
 			}
@@ -139,6 +136,8 @@ func (k Keeper) getTxSigners(txBytes []byte) (map[string]bool, error) {
 	signers := make(map[string]bool, 0)
 	for _, msg := range tx.GetMsgs() {
 		for _, signer := range msg.GetSigners() {
+			// TODO: check for multi-sig accounts
+			// https://github.com/skip-mev/pob/issues/14
 			signers[signer.String()] = true
 		}
 	}
@@ -146,7 +145,7 @@ func (k Keeper) getTxSigners(txBytes []byte) (map[string]bool, error) {
 	return signers, nil
 }
 
-// filterSigners removes any signers fromt he authorizedSigners map that are not in the txSigners map.
+// filterSigners removes any signers from the authorizedSigners map that are not in the txSigners map.
 // This is used to check that all transactions in a bundle are signed by the correct party.
 func filterSigners(authorizedSigners, txSigners map[string]bool) {
 	for signer := range authorizedSigners {
