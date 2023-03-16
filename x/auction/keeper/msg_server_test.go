aa4c73c38266a651da41b6f6b40a331f9230efcc
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-03-16 10:54:17 -0400

    updates

--- x/auction/keeper/msg_server_test.go
+++ x/auction/keeper/msg_server_test.go
@@ -4,6 +4,7 @@ import (
 	"math/rand"
 	"time"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/skip-mev/pob/x/auction/types"
 )
 
@@ -18,6 +19,14 @@ func (suite *KeeperTestSuite) TestMsgAuctionBid() {
 		expectErr bool
 	}{
 		{
+			name: "invalid bidder address",
+			msg: &types.MsgAuctionBid{
+				Bidder: "foo",
+			},
+			malleate:  func() {},
+			expectErr: true,
+		},
+		{
 			name: "too many bundled transactions",
 			msg: &types.MsgAuctionBid{
 				Bidder:       bidder.Address.String(),
@@ -29,6 +38,27 @@ func (suite *KeeperTestSuite) TestMsgAuctionBid() {
 				suite.auctionKeeper.SetParams(suite.ctx, params)
 			},
 			expectErr: true,
+		},
+		{
+			name: "valid bundle with no proposer fee",
+			msg: &types.MsgAuctionBid{
+				Bidder:       bidder.Address.String(),
+				Bid:          sdk.NewCoins(sdk.NewInt64Coin("foo", 1024)),
+				Transactions: [][]byte{{0xFF}, {0xFF}},
+			},
+			malleate: func() {
+				params := types.DefaultParams()
+				params.ProposerFee = sdk.ZeroDec()
+				suite.auctionKeeper.SetParams(suite.ctx, params)
+
+				suite.bankKeeper.EXPECT().SendCoinsFromAccountToModule(
+					suite.ctx,
+					bidder.Address,
+					types.ModuleName,
+					sdk.NewCoins(sdk.NewInt64Coin("foo", 1024)),
+				).Return(nil).AnyTimes()
+			},
+			expectErr: false,
 		},
 	}
 
