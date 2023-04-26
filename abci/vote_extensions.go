c880923c111781000993557296df1d5821e264f3
David Terpay <david.terpay@gmail.com>
2023-04-26 18:10:55 -0400

    nit v2

--- abci/vote_extensions.go
+++ abci/vote_extensions.go
@@ -2,16 +2,18 @@ package abci
 
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
-// ExtendVoteHandler returns the ExtendVoteHandler ABCI handler that extracts the top bidding valid auction transaction
-// from a validator's local mempool and returns it in its vote extension.
+// ExtendVoteHandler returns the ExtendVoteHandler ABCI handler that extracts
+// the top bidding valid auction transaction from a validator's local mempool and
+// returns it in its vote extension.
 func (h *ABCIHandler) ExtendVoteHandler() ExtendVoteHandler {
 	return func(ctx sdk.Context, req *RequestExtendVote) (*ResponseExtendVote, error) {
 		panic("implement me")
 	}
 }
 
-// VerifyVoteExtensionHandler returns the VerifyVoteExtensionHandler ABCI handler that verifies the vote extension
-// returned by the ExtendVoteHandler. In particular, it verifies that the vote extension is a valid auction transaction.
+// VerifyVoteExtensionHandler returns the VerifyVoteExtensionHandler ABCI handler
+// that verifies the vote extension returned by the ExtendVoteHandler.
+// In particular, it verifies that the vote extension is a valid auction transaction.
 func (h *ABCIHandler) VerifyVoteExtensionHandler() VerifyVoteExtensionHandler {
 	return func(ctx sdk.Context, req *RequestVerifyVoteExtension) (*ResponseVerifyVoteExtension, error) {
 		panic("implement me")
