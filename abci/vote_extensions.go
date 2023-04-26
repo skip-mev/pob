45fece640b9701d104b079ab4e9a886858c6aee8
David Terpay <david.terpay@gmail.com>
2023-04-26 17:25:24 -0400

    init vote extension handler

--- abci/vote_extensions.go
+++ abci/vote_extensions.go
@@ -6,7 +6,40 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 // from a validator's local mempool and returns it in its vote extension.
 func (h *ProposalHandler) ExtendVoteHandler() ExtendVoteHandler {
 	return func(ctx sdk.Context, req *RequestExtendVote) (*ResponseExtendVote, error) {
-		panic("implement me")
+		var (
+			voteExtension []byte
+		)
+
+		auctionIterator := h.mempool.AuctionBidSelect(ctx)
+		txsToRemove := make(map[sdk.Tx]struct{})
+
+		// Iterate through auction bids until we find a valid one
+		for auctionIterator != nil {
+			bidTx := auctionIterator.Tx()
+
+			if err := h.verifyTx(ctx, bidTx); err != nil {
+				txsToRemove[bidTx] = struct{}{}
+				continue
+			}
+
+			txBz, err := h.txEncoder(bidTx)
+			if err != nil {
+				txsToRemove[bidTx] = struct{}{}
+				continue
+			}
+
+			voteExtension = txBz
+			break
+		}
+
+		// Remove all invalid auction bids from the mempool
+		for tx := range txsToRemove {
+			h.RemoveTx(tx)
+		}
+
+		return &ResponseExtendVote{
+			VoteExtension: voteExtension,
+		}, nil
 	}
 }
 
