4455a611b770f0aa2b9ce041987a0b55e7dce194
David Terpay <david.terpay@gmail.com>
2023-05-30 19:20:28 -0400

    done with basic stuff

--- blockbuster/lanes/base/abci.go
+++ blockbuster/lanes/base/abci.go
@@ -2,14 +2,14 @@ package base
 
 import sdk "github.com/cosmos/cosmos-sdk/types"
 
-func (l *BaseLane) VerifyTx(sdk.Context, sdk.Tx) error {
-	return nil
-}
-
 func (l *BaseLane) PrepareLane(sdk.Context, int64, map[string][]byte) ([][]byte, error) {
-	return nil, nil
+	panic("implement me")
 }
 
 func (l *BaseLane) ProcessLane(sdk.Context, [][]byte) error {
-	return nil
+	panic("implement me")
+}
+
+func (l *BaseLane) VerifyTx(sdk.Context, sdk.Tx) error {
+	panic("implement me")
 }
